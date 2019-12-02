556697eacc1f08ce8933fc1ebb0bf04ec6a28a40
seokjun <tjrwns8024@naver.com>
2019-12-02 23:21:01 +0900

    [UPDATE] ImageUpload

--- KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/fragment/ChangeProfileFragment.kt
+++ KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/fragment/ChangeProfileFragment.kt
@@ -1,21 +1,32 @@
 package com.example.kakaotalk_dms.ui.fragment
 
+import android.app.Activity.MODE_PRIVATE
 import android.app.Activity.RESULT_OK
+import android.content.Context
 import android.content.Intent
 import android.content.Intent.ACTION_PICK
+import android.content.SharedPreferences
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.net.toFile
 
 import com.example.kakaotalk_dms.R
 import kotlinx.android.synthetic.main.fragment_change_profile.*
+import java.io.File
+import java.io.FileOutputStream
+import kotlin.math.log
 
 class ChangeProfileFragment : Fragment() {
 
     private val GET_GALLERY_IMAGE: Int = 200
+    private lateinit var mPreferences: SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -46,8 +57,13 @@ class ChangeProfileFragment : Fragment() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.data != null) {
-            val selectedImageUri: Uri = data.data!!
-            change_image.setImageURI(selectedImageUri)
+           val selectedImageUri: Uri = data.data!!
+          change_image.setImageURI(selectedImageUri)
+
+//            mPreferences = activity!!.getSharedPreferences("com.example.kakaotalk_dms.ui.fragment", MODE_PRIVATE)
+//            val preferencesEditor:SharedPreferences.Editor = mPreferences.edit()
+//            preferencesEditor.putString("profile_image", selectedImageUri.toString())
+//            preferencesEditor.apply()
         }
 
     }
