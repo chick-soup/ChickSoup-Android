69736835222ab55e5e7134a54bd0dbc301d57930
seokjun <tjrwns8024@naver.com>
2019-12-02 23:20:24 +0900

    [UPDATE] rename

--- KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/fragment/AccountFragment.kt
+++ KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/fragment/AccountFragment.kt
@@ -1,5 +1,6 @@
 package com.example.kakaotalk_dms.ui.fragment
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,8 +9,11 @@ import android.view.ViewGroup
 
 import com.example.kakaotalk_dms.R
 import kotlinx.android.synthetic.main.fragment_account.*
+import org.jetbrains.anko.image
 
 class AccountFragment : Fragment() {
+
+    private lateinit var mPreferences: SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
