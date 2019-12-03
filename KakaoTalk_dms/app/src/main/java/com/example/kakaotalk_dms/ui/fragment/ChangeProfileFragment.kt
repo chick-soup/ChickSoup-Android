6c969f5ebe0e0a3d4d039f2468b49313be6d016f
seokjun <tjrwns8024@naver.com>
2019-12-03 23:26:14 +0900

    [UPDATE] Ui 의 엄청난 변화

--- KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/fragment/ChangeProfileFragment.kt
+++ KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/fragment/ChangeProfileFragment.kt
@@ -1,32 +1,21 @@
 package com.example.kakaotalk_dms.ui.fragment
 
-import android.app.Activity.MODE_PRIVATE
 import android.app.Activity.RESULT_OK
-import android.content.Context
 import android.content.Intent
-import android.content.Intent.ACTION_PICK
-import android.content.SharedPreferences
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.net.toFile
 
 import com.example.kakaotalk_dms.R
 import kotlinx.android.synthetic.main.fragment_change_profile.*
-import java.io.File
-import java.io.FileOutputStream
-import kotlin.math.log
 
 class ChangeProfileFragment : Fragment() {
 
     private val GET_GALLERY_IMAGE: Int = 200
-    private lateinit var mPreferences: SharedPreferences
+    //private lateinit var mPreferences: SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,13 +28,13 @@ class ChangeProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        change_profile_back.setOnClickListener {
+        change_profile_back_btn.setOnClickListener {
             val transaction = activity?.supportFragmentManager?.beginTransaction()
                 ?.setCustomAnimations(R.anim.fade_in, R.anim.fade_out)
-            transaction?.replace(R.id.main_frame, SettingFragment())
+            transaction?.replace(R.id.main_frame, AccountFragment())
             transaction?.commit()
         }
-        change_image.setOnClickListener {
+        change_profile_image.setOnClickListener {
             val intent: Intent = Intent(Intent.ACTION_PICK)
             intent.setDataAndType(
                 android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
