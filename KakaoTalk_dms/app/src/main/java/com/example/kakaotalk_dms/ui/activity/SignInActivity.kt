67a203490ba889b0620f7707920c49500b446bba
seokjun <tjrwns8024@naver.com>
2019-12-05 10:20:54 +0900

    [ADD] 로직 구현

--- KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/activity/SignInActivity.kt
+++ KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/activity/SignInActivity.kt
@@ -2,9 +2,11 @@ package com.example.kakaotalk_dms.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.kakaotalk_dms.R
 import kotlinx.android.synthetic.main.activity_signin.*
 import org.jetbrains.anko.startActivity
+import kotlin.math.sign
 
 class SignInActivity : AppCompatActivity() {
 
@@ -12,11 +14,28 @@ class SignInActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
 
+        signin_idInputLayout.isErrorEnabled = true
+
         login_btn.setOnClickListener {
-            startActivity<MainActivity>()
+            signin_email_check(signin_id.text.toString())
         }
         go_signup.setOnClickListener {
             startActivity<SignUp1Activity>()
         }
+
+    }
+
+    fun signin_email_check(signin_id: String): Boolean {
+        if (android.util.Patterns.EMAIL_ADDRESS.matcher(signin_id).matches()) {
+            signin_idInputLayout.error = ""
+            Log.d("success Login", "god")
+            startActivity<MainActivity>()
+            return true
+        } else {
+            signin_idInputLayout.error = "       이메일 형식이  맞지 않습니다"
+            Log.d("fail","failure")
+            return false
+        }
     }
 }
+
--- KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/activity/SignUp1Activity.kt
+++ KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/activity/SignUp1Activity.kt
@@ -12,8 +12,40 @@ class SignUp1Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up1)
 
+        signup_idInputLayout.isErrorEnabled = true
+        signup_passwordInputLayout.isErrorEnabled = true
+        codeTextInputLayout.isErrorEnabled = true
+
         signup_next_btn.setOnClickListener {
-            startActivity<SignUp2Activity>()
+            val isEmailError:Boolean = emailError(signup_id.text.toString())
+            val isPasswordEqual:Boolean = passwordCheck(signup_password.text.toString(), password_check.text.toString())
+
+            if(isEmailError&&isPasswordEqual)
+                startActivity<SignUp2Activity>()
+        }
+        sendEmail_btn.setOnClickListener {
+            emailError(signup_id.text.toString())
         }
     }
+    fun emailError(signup_id:String): Boolean {
+        if(android.util.Patterns.EMAIL_ADDRESS.matcher(signup_id).matches()){
+            signup_idInputLayout.error = ""
+            return true
+        }
+        else{
+            signup_idInputLayout.error = "        이메일 형식이 맞지 않습니다."
+            return false
+        }
+    }
+    fun passwordCheck(signup_password:String, password_check: String):Boolean{
+        if(signup_password.equals(password_check)){
+            check_pw.error = ""
+            return true
+        }
+        else{
+            check_pw.error = "      비밀번호가 일치하지 않습니다."
+            return false
+        }
+    }
+
 }
