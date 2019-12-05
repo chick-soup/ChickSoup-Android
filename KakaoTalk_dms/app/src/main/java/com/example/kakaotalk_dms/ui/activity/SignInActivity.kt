99ce9bb2918ba1e5274d4dd56073df8d7da2c327
seokjun <tjrwns8024@naver.com>
2019-12-05 10:33:08 +0900

    [UPDATE] 로직 삭제

--- KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/activity/SignInActivity.kt
+++ KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/activity/SignInActivity.kt
@@ -17,25 +17,12 @@ class SignInActivity : AppCompatActivity() {
         signin_idInputLayout.isErrorEnabled = true
 
         login_btn.setOnClickListener {
-            signin_email_check(signin_id.text.toString())
+            startActivity<MainActivity>()
         }
         go_signup.setOnClickListener {
             startActivity<SignUp1Activity>()
         }
 
-    }
-
-    fun signin_email_check(signin_id: String): Boolean {
-        if (android.util.Patterns.EMAIL_ADDRESS.matcher(signin_id).matches()) {
-            signin_idInputLayout.error = ""
-            Log.d("success Login", "god")
-            startActivity<MainActivity>()
-            return true
-        } else {
-            signin_idInputLayout.error = "       이메일 형식이  맞지 않습니다"
-            Log.d("fail","failure")
-            return false
-        }
     }
 }
 
