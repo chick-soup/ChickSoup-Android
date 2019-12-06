2082c3c6b815041e1640ac944dacd8a7bad43a4a
seokjun <tjrwns8024@naver.com>
2019-12-06 10:30:59 +0900

    [ADD] SignUp 통신

--- KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/activity/SignUp1Activity.kt
+++ KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/activity/SignUp1Activity.kt
@@ -2,11 +2,18 @@ package com.example.kakaotalk_dms.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.kakaotalk_dms.R
+import android.util.Log
+import com.example.kakaotalk_dms.*
 import kotlinx.android.synthetic.main.activity_sign_up1.*
 import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SignUp1Activity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +31,12 @@ class SignUp1Activity : AppCompatActivity() {
                 startActivity<SignUp2Activity>()
         }
         sendEmail_btn.setOnClickListener {
-            emailError(signup_id.text.toString())
+            val isEmailError:Boolean = emailError(signup_id.text.toString())
+            if(isEmailError)
+                signup_post()
+        }
+        sendCode_btn.setOnClickListener {
+            auth_code()
         }
     }
     fun emailError(signup_id:String): Boolean {
@@ -46,6 +58,40 @@ class SignUp1Activity : AppCompatActivity() {
             check_pw.error = "      비밀번호가 일치하지 않습니다."
             return false
         }
+    }
+    fun signup_post(){
+        val userEmail:String = signup_id.text.toString()
+        val call:Call<Jsonparse> = Retrofit().service.sendEmail(SignUp(userEmail))
+
+        call.enqueue(object:Callback<Jsonparse>{
+            override fun onFailure(call: Call<Jsonparse>, t: Throwable) {
+                Log.e("fail",t.localizedMessage)
+            }
+            override fun onResponse(call: Call<Jsonparse>, response: Response<Jsonparse>) {
+                toast("이메일 인증코드 발송 ")
+                Log.d("success", response.code().toString())
+            }
+        })
+    }
+
+    fun auth_code() {
+        val userEmail: String = signup_id.text.toString()
+        val auth_code: String = check_code.text.toString()
+        val call: Call<Jsonparse> = Retrofit().service.sendCode(SignUp(userEmail, auth_code))
+
+        call.enqueue(object : Callback<Jsonparse> {
+            override fun onFailure(call: Call<Jsonparse>, t: Throwable) {
+                Log.e("auth_code_error", t.printStackTrace().toString())
+            }
+
+            override fun onResponse(call: Call<Jsonparse>, response: Response<Jsonparse>) {
+                if (response.code() == 200) {
+                    signup_id.isFocusable = false
+                    signup_id.isClickable = false
+                    toast("인증성공")
+                }
+            }
+        })
     }
 
 }
