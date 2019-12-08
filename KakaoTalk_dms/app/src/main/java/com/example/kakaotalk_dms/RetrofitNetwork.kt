d2ce843765b11689b78a08b83ec999310d818262
seokjun <tjrwns8024@naver.com>
2019-12-08 23:00:25 +0900

    [ADD] SignUp2 추가

--- KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/RetrofitNetwork.kt
+++ KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/RetrofitNetwork.kt
@@ -2,15 +2,19 @@ package com.example.kakaotalk_dms
 
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface RetrofitNetwork {
     @POST("/email/check")
-    fun sendEmail(@Body signup:SignUp): Call<SignUp>
+    fun sendEmail(@Body signup1:SignUp1): Call<SignUp1>
 
     @POST("/email/auth")
-    fun sendCode(@Body signup: SignUp): Call<SignUp>
+    fun sendCode(@Body signup1: SignUp1): Call<SignUp1>
 
     @POST("/user/signup")
-    fun sendPassword(@Body signup: SignUp): Call<SignUp>
+    fun sendPassword(@Body signup1: SignUp1): Call<SignUp1>
+
+    @POST("/user/signup/profile")
+    fun sendProfile(@Header("access_token") authorization:String, @Body signUp2: SignUp2):Call<SignUp2>
     }
---
+++ KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/SignUp2.kt
@@ -0,0 +1,12 @@
+package com.example.kakaotalk_dms
+
+import com.google.gson.annotations.SerializedName
+
+class SignUp2(
+    @SerializedName("nickname")
+    val nickname:String,
+    @SerializedName("profile")
+    val profile:String
+) {
+
+}
