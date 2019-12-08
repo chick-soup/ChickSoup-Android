459d3742a3fe2b91b153a87eb1caee52dda8156c
seokjun <tjrwns8024@naver.com>
2019-12-08 22:59:34 +0900

    [ADD] SignUp1 추가

--- KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/SignUp1.kt
+++ KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/SignUp1.kt
@@ -2,7 +2,7 @@ package com.example.kakaotalk_dms
 
 import com.google.gson.annotations.SerializedName
 
-class SignUp(
+class SignUp1(
     @SerializedName("email")
     val userEmail:String,
     @SerializedName("auth_code")
@@ -10,6 +10,9 @@ class SignUp(
     @SerializedName("password")
     val userPassword:String
 ){
+    @SerializedName("access_token")
+    lateinit var userToken:String
+
     constructor(userEmail: String) : this(userEmail,"","")
     constructor(userEmail: String, userPassword:String):this(userEmail,"",userPassword)
 }
