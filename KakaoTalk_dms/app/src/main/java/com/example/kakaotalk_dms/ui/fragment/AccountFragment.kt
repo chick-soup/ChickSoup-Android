77a5067d544b3afe0e685bfe1cdb9e7bdc621de3
seokjun <tjrwns8024@naver.com>
2019-12-10 23:12:26 +0900

    [ADD] checkId & getProfile

--- KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/fragment/AccountFragment.kt
+++ KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/fragment/AccountFragment.kt
@@ -2,14 +2,21 @@ package com.example.kakaotalk_dms.ui.fragment
 
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.example.kakaotalk_dms.R
+import com.example.kakaotalk_dms.Retrofit
+import com.example.kakaotalk_dms.data.Profile
+import com.example.kakaotalk_dms.util.UtilClass
 import kotlinx.android.synthetic.main.fragment_account.*
 import org.jetbrains.anko.image
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class AccountFragment : Fragment() {
 
@@ -25,21 +32,43 @@ class AccountFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        getProfileInfo()
+
         account_setting.setOnClickListener {
             val transaction = activity!!.supportFragmentManager.beginTransaction().setCustomAnimations(R.anim.slide_in_bottom,R.anim.fade_out)
             transaction.replace(R.id.main_frame,ChangeProfileFragment())
             transaction.commit()
         }
 
-        val args: Bundle? = arguments
-        if(args != null)
-        {
-            val mNick: String? = args.getString("nick")
-            val state_message:String? = args.getString("state_message")
+//        val args: Bundle? = arguments
+//        if(args != null)
+//        {
+//            val mNick: String? = args.getString("nick")
+//            val state_message:String? = args.getString("state_message")
+//
+//            account_name.text = mNick
+//            account_message.text = state_message
+//        }
+    }
+    fun getProfileInfo(){
+        val jwt = UtilClass.getToken(activity!!.applicationContext)
+        val call = Retrofit().service.getProfile(jwt,"","","")
 
-            account_name.text = mNick
-            account_message.text = state_message
-        }
+        call.enqueue(object : Callback<Profile> {
+            override fun onFailure(call: Call<Profile>, t: Throwable) {
+             Log.e("can't getProfile",t.message.toString())
+            }
+
+            override fun onResponse(call: Call<Profile>, response: Response<Profile>) {
+                if(response.code() == 200){
+                    account_name.setText(response.body()?.userNick)
+                    account_message.setText(response.body()?.userMessage)
+
+                    Log.d("imageId",response.body()?.imageId.toString())
+                }
+            }
+
+        })
     }
 
 }
