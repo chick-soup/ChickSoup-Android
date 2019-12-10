305cf0fd039945c98d08897a422a9989e120aff1
seokjun <tjrwns8024@naver.com>
2019-12-10 23:11:44 +0900

    [ADD] 로그인 post

--- KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/activity/SignInActivity.kt
+++ KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/activity/SignInActivity.kt
@@ -1,17 +1,18 @@
 package com.example.kakaotalk_dms.ui.activity
 
-import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.example.kakaotalk_dms.*
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kakaotalk_dms.R
+import com.example.kakaotalk_dms.Retrofit
+import com.example.kakaotalk_dms.data.SignIn
+import com.example.kakaotalk_dms.util.UtilClass
 import kotlinx.android.synthetic.main.activity_signin.*
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import kotlin.math.sign
 
 class SignInActivity : AppCompatActivity() {
 
@@ -28,11 +29,15 @@ class SignInActivity : AppCompatActivity() {
 
     }
     fun signin_post(){
-        Log.d("come","ddd")
         val userEmail = signin_id.text.toString()
         val userPassword = signin_password.text.toString()
 
-        val call= Retrofit().service.sendLogin(SignIn(userEmail, userPassword))
+        val call= Retrofit().service.sendLogin(
+            SignIn(
+                userEmail,
+                userPassword
+            )
+        )
 
         call.enqueue(object: Callback<SignIn>{
             override fun onFailure(call: Call<SignIn>, t: Throwable) {
@@ -41,18 +46,13 @@ class SignInActivity : AppCompatActivity() {
 
             override fun onResponse(call: Call<SignIn>, response: Response<SignIn>) {
                 Log.d("signin_success",response.code().toString())
-                toast("로그인 성공")
+
                 if(response.code() == 200) {
+                    toast("로그인 성공")
                     startActivity<MainActivity>()
                     val jwt: String = response.body()!!.userToken
+                    UtilClass.saveToken(applicationContext,jwt)
 
-                    val prefs = getSharedPreferences("com.example.kakaotalk_dms.ui.SignInActivity",MODE_PRIVATE)
-                    val editor: SharedPreferences.Editor = prefs.edit()
-                    editor.putString("login_token",jwt)
-                    editor.apply()
-
-                    val login_jwt = prefs.getString("login_token","")
-                    Log.d("login_token",login_jwt.toString())
                 }
             }
 
