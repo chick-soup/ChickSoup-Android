d61f74556eab0ccab8b4db7f72f8f72891b2c650
seokjun <tjrwns8024@naver.com>
2019-12-11 22:18:16 +0900

    [ADD] getProfile

--- KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/RetrofitNetwork.kt
+++ KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/RetrofitNetwork.kt
@@ -1,5 +1,9 @@
 package com.example.kakaotalk_dms
 
+import com.example.kakaotalk_dms.data.KakaoId
+import com.example.kakaotalk_dms.data.Profile
+import com.example.kakaotalk_dms.data.SignIn
+import com.example.kakaotalk_dms.data.SignUp1
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Call
@@ -17,8 +21,22 @@ interface RetrofitNetwork {
 
     @Multipart
     @POST("/signup/profile")
-    fun sendProfile(@Header("Authorization") authorization: String, @Part file: MultipartBody.Part, @Part("nickname") nick:RequestBody): Call<UploadSuccess>
+    fun sendFirstProfile(@Header("Authorization") authorization: String, @Part file: MultipartBody.Part, @Part("nickname") nick:RequestBody): Call<UploadSuccess>
 
     @POST("/login")
     fun sendLogin(@Body signIn: SignIn):Call<SignIn>
+
+    @GET("/users/my/kakao-id")
+    fun getKakaoId(@Header("Authorization") authorization: String) :Call<KakaoId>
+
+    @GET("/users/my/profile")
+    fun getProfile(@Header("Authorization") authorization: String,
+                   @Query("id") id:String,
+                   @Query("nickname") nick:String,
+                   @Query("status_message") message:String): Call<Profile>
+
+    @Multipart
+    @PUT("/users/my/profile")
+    fun changeProfile(@Header("Authorization") authorization: String,
+                      @Part("nickname") nick:RequestBody): Call<Profile>
 }
