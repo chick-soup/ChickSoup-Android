4eb896bb4f6de5557822a74d67b1e98f0aff3c76
seokjun <tjrwns8024@naver.com>
2019-12-11 22:15:01 +0900

    [UPDATE] alert dialog로 바꿈

--- KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/fragment/ChangeIdDialogFragment.kt
+++ KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/fragment/ChangeIdDialogFragment.kt
@@ -1,10 +1,25 @@
 package com.example.kakaotalk_dms.ui.fragment
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Context.CLIPBOARD_SERVICE
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.DialogFragment
+import com.example.kakaotalk_dms.R
+import com.example.kakaotalk_dms.Retrofit
+import com.example.kakaotalk_dms.data.KakaoId
+import com.example.kakaotalk_dms.util.UtilClass
+import kotlinx.android.synthetic.main.dialog_id_fragment.*
+import org.jetbrains.anko.toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class ChangeIdDialogFragment : DialogFragment() {
@@ -20,20 +35,56 @@ class ChangeIdDialogFragment : DialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         val v = inflater.inflate(
-            com.example.kakaotalk_dms.R.layout.dialog_id_fragment,
+            R.layout.dialog_id_fragment,
             container,
             false
         )
+        getProfile()
 
         return v
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkId_cancel_btn.setOnClickListener {
+            dialog?.cancel()
+        }
+        clipboard.setOnClickListener {
+            val checkId = checkId.text.toString()
+            val clipboardManager= activity!!.getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+            val clipData = ClipData.newPlainText("checkId",checkId)
+            clipboardManager.setPrimaryClip(clipData)
+
+            activity!!.toast("클립보드에 복사되었습니다.")
+        }
+    }
+
     override fun onResume() {
         super.onResume()
+        dialog?.setCancelable(false)
         val params = dialog!!.window!!.attributes
         params.width = ViewGroup.LayoutParams.MATCH_PARENT
         params.height = ViewGroup.LayoutParams.WRAP_CONTENT
         dialog!!.window!!.attributes = params as android.view.WindowManager.LayoutParams
+    }
+    fun getProfile(){
+        val current_jwt= UtilClass.getToken(activity!!.applicationContext)
+
+        val call = Retrofit().service.getKakaoId(current_jwt)
+
+        call.enqueue(object : Callback<KakaoId> {
+            override fun onFailure(call: Call<KakaoId>, t: Throwable) {
+                Log.e("error",t.message.toString())
+            }
+            override fun onResponse(call: Call<KakaoId>, response: Response<KakaoId>) {
+                Log.d("get_jwt",current_jwt)
+                Log.d("success_getId",response.code().toString())
+                if(response.code() == 200){
+                    checkId.setText(response.body()?.userKakaoId)
+                }
+            }
+
+        })
     }
 
 }
--- KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/fragment/SettingFragment.kt
+++ KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/fragment/SettingFragment.kt
@@ -2,6 +2,7 @@ package com.example.kakaotalk_dms.ui.fragment
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,7 @@ import android.view.ViewGroup
 
 import com.example.kakaotalk_dms.R
 import com.example.kakaotalk_dms.ui.activity.SignInActivity
+import com.example.kakaotalk_dms.util.UtilClass
 import kotlinx.android.synthetic.main.fragment_setting.*
 
 class SettingFragment : Fragment() {
@@ -30,9 +32,8 @@ class SettingFragment : Fragment() {
 
         }
         check_id.setOnClickListener{
-            val transaction = activity?.supportFragmentManager?.beginTransaction()?.setCustomAnimations(R.anim.slide_in_bottom,R.anim.fade_out)
-            transaction?.replace(R.id.main_frame,CheckIdFragment())
-            transaction?.commit()
+            val changeIdDialogFragment = ChangeIdDialogFragment.getInstance()
+            changeIdDialogFragment.show(activity!!.supportFragmentManager, ChangeIdDialogFragment.TAG_CHANGE_ID_DIALOG)
         }
         ban_list.setOnClickListener{
             val transaction = activity?.supportFragmentManager?.beginTransaction()?.setCustomAnimations(R.anim.slide_in_bottom,R.anim.fade_out)
@@ -40,6 +41,7 @@ class SettingFragment : Fragment() {
             transaction?.commit()
         }
         logout_card.setOnClickListener {
+            UtilClass.clearToken(activity!!.applicationContext)
             activity!!.finish()
         }
     }
