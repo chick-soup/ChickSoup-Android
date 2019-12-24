6a2b36d4c278c5012abc6f14161a5eb41609da54
seokjun <tjrwns8024@naver.com>
2019-12-24 23:31:11 +0900

    [ADD]extra_view
    extra_view 추가해서 plus_media_btn누르면 visible 설정하는 코드 추가

--- KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/activity/ChatRoomActivity.kt
+++ KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/activity/ChatRoomActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.view.WindowManager
+import android.view.animation.AlphaAnimation
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
@@ -18,12 +20,14 @@ import kotlinx.android.synthetic.main.chatroom_appbar.*
 import org.jetbrains.anko.toast
 
 
-class ChatRoomActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class ChatRoomActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener,View.OnClickListener {
     private var i = 0
     private val drawerLayout: DrawerLayout? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_room)
+
+        extra_view.visibility = View.GONE
         setSupportActionBar(chatroom_bar)
         initLayout()
 
@@ -35,34 +39,46 @@ class ChatRoomActivity : AppCompatActivity(), NavigationView.OnNavigationItemSel
             val drawerLayout = findViewById<DrawerLayout>(R.id.dl_main_drawer_root)
             drawerLayout.openDrawer(GravityCompat.END)
         }
-        plus_media_btn.setOnClickListener {
-            Log.d("click","yellow")
-            i = 1 - i
-            if(i==1) plus_media_btn.setImageResource(R.drawable.yellow_plus_media)
-            else plus_media_btn.setImageResource(R.drawable.plus_media)
+        plus_media_btn.setOnClickListener(this)
+    }
+    override fun onClick(v: View?){
+        when(v?.id){
+            R.id.plus_media_btn->{
+                Log.d("click", "$i")
+                i = 1 - i
+                if (i == 1) {
+                    plus_media_btn.setImageResource(R.drawable.yellow_plus_media)
+                    extra_view.visibility = View.VISIBLE
+                } else {
+                    plus_media_btn.setImageResource(R.drawable.plus_media)
+                    extra_view.visibility = View.GONE
+                }
+            }
         }
     }
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
+        when (item.itemId) {
             R.id.menuitem1 -> toast("1번 클릭")
             R.id.menuitem2 -> toast("2번 클릭")
         }
         drawerLayout?.closeDrawer(GravityCompat.END)
         return false
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val drawerLayout = findViewById<DrawerLayout>(R.id.dl_main_drawer_root)
-        when(item.itemId){
-            R.id.hamburgerIcon->{
-                Log.d("hamburger","eat")
+        when (item.itemId) {
+            R.id.hamburgerIcon -> {
+                Log.d("hamburger", "eat")
                 drawerLayout?.openDrawer(GravityCompat.END)
                 return true
             }
         }
-        return  super.onOptionsItemSelected(item)
+        return super.onOptionsItemSelected(item)
     }
-    private fun initLayout(){
-        Log.d("initLayout","넘어왔네요")
+
+    private fun initLayout() {
+        Log.d("initLayout", "넘어왔네요")
         //val drawerLayout = findViewById<DrawerLayout>(R.id.dl_main_drawer_root)
 
         //val chatroom_bar = findViewById<Toolbar>(R.id.chatroom_bar)
@@ -70,11 +86,12 @@ class ChatRoomActivity : AppCompatActivity(), NavigationView.OnNavigationItemSel
         ab?.title = null
         ab?.setDisplayShowCustomEnabled(true)
 
-       // val drawerToggle = ActionBarDrawerToggle(this,drawerLayout,chatroom_bar,
-         //   R.string.drawer_open, R.string.drawer_close)
+        // val drawerToggle = ActionBarDrawerToggle(this,drawerLayout,chatroom_bar,
+        //   R.string.drawer_open, R.string.drawer_close)
 //        drawerLayout.addDrawerListener(drawerToggle)
 //        drawerToggle.syncState()
     }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.chatroom_menu, menu)
         return true
