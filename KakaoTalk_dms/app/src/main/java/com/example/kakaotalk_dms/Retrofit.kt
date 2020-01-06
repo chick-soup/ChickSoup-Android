6085bd3ab215af8164a55fc5474f46d0707624f3
seokjun <tjrwns8024@naver.com>
2020-01-06 12:25:50 +0900

    [UPDATE] host 주소 변경

--- KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/Retrofit.kt
+++ KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/Retrofit.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class Retrofit {
     val retrofit = Retrofit.Builder()
-        .baseUrl("http://ec2-13-209-99-114.ap-northeast-2.compute.amazonaws.com:8080/")
+        .baseUrl("http://ec2-13-125-190-40.ap-northeast-2.compute.amazonaws.com:8080/")
         .addConverterFactory(
             GsonConverterFactory.create()
         ).build()
